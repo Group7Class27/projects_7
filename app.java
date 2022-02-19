93cd79738a484ad50db05c4fc8cd9096043e6e85
Gislaine <mgislaine2@yahoo.fr>
2022-02-19 00:39:54 -0600

    My first commit

---
+++ app.java
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter your username"
+read name
+echo "Please enter your password"
+read -s password
+echo "welcome to the zelle application" $name 
+echo "You have entered the valid password"
+echo "Have a great day"
+date
